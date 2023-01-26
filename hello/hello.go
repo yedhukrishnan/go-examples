6a57d7ca16bbcdbf6babff554272c769caafb097
Yedhu Krishnan <yedhukrishnan@yahoo.com>
2023-01-26 20:50:09 +0530

    Try maps and slices

--- hello/hello.go
+++ hello/hello.go
@@ -10,10 +10,20 @@ func main() {
   log.SetPrefix("greetings: ")
   log.SetFlags(0)
   // Get a greeting message and print it.
-  message, err := greetings.Hello("")
+  message, err := greetings.Hello("Yedhu")
   if err!= nil {
     log.Fatal(err)
   }
 
   fmt.Println(message)
+
+  names := []string{"YK", "Yedhu", "John Doe"}
+  messages, err := greetings.Hellos(names)
+
+  if err != nil {
+    log.Fatal(err)
+  }
+
+  fmt.Println(messages)
+  fmt.Println(messages["YK"])
 }
